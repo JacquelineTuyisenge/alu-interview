40b7b770772158858036c785725af3de65deaad5
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-06-07 14:26:43 +0200

    Update 0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+"""
+    Calculate how many square units of water will be retained after it rains.
+"""
 
+def rain(walls):
+  """
+      Walls is list of non negative integers.
+  """
+  walls >= 0
+  for x in walls:
+  walls = [1, x-1]
+  
+  
+ 

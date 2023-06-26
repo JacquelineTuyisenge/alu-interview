e674a5457970bd1afde583038ac0d1bdf61e90d8
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-06-26 14:55:24 +0200

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # creating function that return list of lists of integers in pascal triangle
 
+import pascal_triangle
+
+def pascal_triangle(n):
+  for( x, prev, next ) in n:
+  x = [1, n]:
+  if( x = 1, x <= 5, x++ ):
+      print([1, (prev + next)]):
+else:
+    print([])
